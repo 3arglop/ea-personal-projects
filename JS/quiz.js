9278f83438b12c276b61004e5a1655ca5ae56690
3arglop <estefania.arguetalopez13@gmail.com>
2022-10-06 17:45:50 -0500

    Created a quiz-data JSON file. New project: Quiz App

---
+++ JS/quiz.js
@@ -0,0 +1,59 @@
+console.log("Quiz Time");
+
+const quizData = [
+    {
+        question: "In Alabama, dressing up as or pretending to be a priest, nun, rabbi or other members of the clergy is illegal.",
+        a: "True",
+        b: "False",
+        correct: "A"
+    },
+    {
+        question: "If you’re a bingo lover, you’ll hate to find out that a game cannot last more than five hours in Texas.",
+        a: "True",
+        b: "False",
+        correct: "B"
+    },
+    {
+        question: "You’re not allowed to drive a black car on a Sunday in Denver, Colorado.",
+        a: "False",
+        b: "True",
+        correct: "B"
+    },
+    {
+        question: "If in Louisiana, don’t try to send anyone a surprise pizza or you’ll have to pay heavily for this appalling gesture – a whole $500 fine is coming your way.",
+        a: "True",
+        b: "False",
+        correct: "B"
+    },
+    {
+        question: "It’s illegal to drive while your dog is tethered to your car in Los Angeles, California.",
+        a: "False",
+        b: "True",
+        correct: "A"
+    },
+    {
+        question: "It’s illegal to run out of gas in Youngstown, Ohio.",
+        a: "False",
+        b: "True",
+        correct: "B"
+    },
+    {
+        question: "If you’re found wearing a bulletproof vest while committing a crime in New Jersey, you will not only be charged with the crime but have to be charged additionally for trying to protect yourself against bullet wounds.",
+        a: "True",
+        b: "False",
+        correct: "A"
+    },
+    {
+       question: "If you’re over 14 years old you cannot trick-or-treat in Chesapeake City, Virginia.",
+        a: "True",
+        b: "False",
+        correct: "A"
+    },
+    {
+        question: "In Utah, women must get permission from their husbands if they want to get false teeth.",
+        a: "False",
+        b: "True",
+        correct: "A"
+    },
+
+]
