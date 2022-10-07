cc98d0b66d84b4a0cdd8bf3cf8026ee38e3e8c34
3arglop <estefania.arguetalopez13@gmail.com>
2022-10-07 09:25:16 -0500

    Quiz App: working on the functionality. Mananged to get the data to show and my button works

--- JS/quiz.js
+++ JS/quiz.js
@@ -1,59 +1,44 @@
 console.log("Quiz Time");
 
-const quizData = [
-    {
-        question: "In Alabama, dressing up as or pretending to be a priest, nun, rabbi or other members of the clergy is illegal.",
-        a: "True",
-        b: "False",
-        correct: "A"
-    },
-    {
-        question: "If you’re a bingo lover, you’ll hate to find out that a game cannot last more than five hours in Texas.",
-        a: "True",
-        b: "False",
-        correct: "B"
-    },
-    {
-        question: "You’re not allowed to drive a black car on a Sunday in Denver, Colorado.",
-        a: "False",
-        b: "True",
-        correct: "B"
-    },
-    {
-        question: "If in Louisiana, don’t try to send anyone a surprise pizza or you’ll have to pay heavily for this appalling gesture – a whole $500 fine is coming your way.",
-        a: "True",
-        b: "False",
-        correct: "B"
-    },
-    {
-        question: "It’s illegal to drive while your dog is tethered to your car in Los Angeles, California.",
-        a: "False",
-        b: "True",
-        correct: "A"
-    },
-    {
-        question: "It’s illegal to run out of gas in Youngstown, Ohio.",
-        a: "False",
-        b: "True",
-        correct: "B"
-    },
-    {
-        question: "If you’re found wearing a bulletproof vest while committing a crime in New Jersey, you will not only be charged with the crime but have to be charged additionally for trying to protect yourself against bullet wounds.",
-        a: "True",
-        b: "False",
-        correct: "A"
-    },
-    {
-       question: "If you’re over 14 years old you cannot trick-or-treat in Chesapeake City, Virginia.",
-        a: "True",
-        b: "False",
-        correct: "A"
-    },
-    {
-        question: "In Utah, women must get permission from their husbands if they want to get false teeth.",
-        a: "False",
-        b: "True",
-        correct: "A"
-    },
+//CALLING THE DOM: HELLO HTML
+const questionElement = document.getElementById('question');
+const aText = document.getElementById('a-text');
+const bText = document.getElementById('b-text');
+const submit = document.getElementById('btn');
 
-]
+//INITIALIZING THE QUIZ TO THE FIRST QUESTION
+let currentQuestion = 0;
+
+//CALLING MY JSON FILE: HELLO DATA
+const fetchQuizData = () => {
+    const URL = "../DATA/quiz-data.json"
+
+    fetch(URL)
+        .then(response => {
+            console.log(response);
+            // const data = response.json();
+            if (!response.ok) {
+                throw Error('ERROR');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            const currentQuizData = data[currentQuestion];
+            questionElement.innerText = currentQuizData.question ;
+            aText.innerText = currentQuizData.a;
+            bText.innerText = currentQuizData.b;
+        });
+}
+
+fetchQuizData();
+
+//CONNECTING THE DOM WITH EVENT LISTENERS
+submit.addEventListener("click", () => {
+    currentQuestion++;
+
+    // if(currentQuestion > currentQuizData.length) {
+        fetchQuizData();
+    // }
+
+});
