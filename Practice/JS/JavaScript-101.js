95fb2b36c84155edb01cd7269f80ef17d99d4561
3arglop <estefania.arguetalopez13@gmail.com>
2022-11-07 12:31:09 -0600

    JS JS JS JS JS

--- Practice/JS/JavaScript-101.js
+++ Practice/JS/JavaScript-101.js
@@ -120,7 +120,7 @@ console.log(sumArray([50, 50, 1]));
 
 //TODO: Hint: store the days of the week in an array, or use an object with numerical keys.  When the function is called, plug the number into the array/object you've created to retrieve the corresponding day name and then return that value.
 
-const daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thrusday", "Friday", "Saturday", "Sunday"];
+const daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
 console.log(daysOfWeek);
 console.table(daysOfWeek);
@@ -129,10 +129,10 @@ console.log(daysOfWeek[4]);
 
 
 function returnDay(arr, num) {
-    if(arr[num] > 1 || arr[num] < 7) {
+    if(num > 1 || num < 7) {
         return null;
     }
-    return arr[num];
+    return arr[num + 1];
 }
 
 console.log(returnDay(daysOfWeek, 8));
@@ -378,3 +378,256 @@ yell("ILY");
 yell("😜");
 
 //
+const emojis = ["🤣", "🥰", "😠", "🥸", "😭", "😕", "🤩", "😡"];
+
+emojis.forEach(function(i) {
+    console.log(`${i} is an emoji!`);
+});
+
+//ANOTHER WAY OF CREATING A FOREACH LOOP WITHOUT HAVING TO ASSIGNED IT INTO A FUNCTION. HOWEVER, IT'S NOT ALWAYS THE CASE OF USING THIS METHOD COS IT WILL NOT ALWAYS WORK!!! GET USED TO USING FOR EACH!!!
+for(let element of emojis) {
+    console.log(`${element} is MY MOOD!!!!`);
+}
+
+const nums = [100, 76, 675276, 8, 886643, 54232794, 100, 3, 99, 85];
+
+nums.forEach(function(x) {
+    if(x % 2 === 0) {
+        console.log(`${x} is an EVEN number`);
+    }
+    let multiply = x * 2;
+    console.log(`${x} * 2 = ${multiply}`);
+})
+
+//
+const  composers = [
+    {
+        name: "Mozart",
+        score: 99
+    },
+    {
+        name: "Beethoven",
+        score: 100
+    },
+    {
+        name: "Chopin",
+        score: 89
+    },
+    {
+        name: "Bach",
+        score: 90
+    },
+    {
+        name: "Wagner",
+        score: 76
+    }
+]
+
+composers.forEach(function(composer) {
+    console.log(`${composer.name} -- ${composer.score}/100`);
+});
+
+//
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const doubles = numbers.map(function(num) {
+    return num * 2; //CONSOLE.LOG WILL RETURN UNDEFINED
+});
+
+console.log(numbers);
+console.log(doubles);
+
+const textSlang = ["lol", "lmao", "rolf", "hahah", "ttyl", "brb", "asap", "iykyk"];
+
+const textSlangYelling = textSlang.map(function(text) {
+    return text.toUpperCase();
+});
+
+console.log(textSlang);
+console.log(textSlangYelling);
+
+//
+const heroes = [
+    {
+        name: "Captain America",
+        power: "Soldier Serum",
+        gender: "Male",
+        age: 35
+    },
+    {
+        name: "Iron-Man",
+        power: "Rich",
+        gender: "Male",
+        age: 40
+    },
+    {
+        name: "Hulk",
+        power: "Radiation",
+        gender: "Male",
+        age: 33
+    },
+    {
+        name: "Black Widow",
+        power: "Assassin",
+        gender: "Female",
+        age: 29
+    },
+    {
+        name: "Captain Marvel",
+        power: "Infinity Stone",
+        gender: "Female",
+        age: 32
+    },
+    {
+        name: "Mr. Fantastic",
+        power: "Elasticity",
+        gender: "Male",
+        age: 45
+    }
+]
+console.table(heroes);
+
+const heroNames = heroes.map(function(name) {
+   return name.name;
+});
+
+console.table(heroNames);
+
+const heroPowers = heroes.map(function(power) {
+    return power.power;
+});
+
+console.table(heroPowers);
+
+const heroGenAndAge = heroes.map(function(hero) {
+    const genderAndAge = {
+        gender: `${hero.gender}`,
+        age: `${hero.age}`
+    }
+    return genderAndAge;
+})
+
+console.log(heroGenAndAge);
+console.table(heroGenAndAge);
+
+//
+const fullNames = [
+    {first: 'Albus', last: 'Dumbledore'},
+    {first: 'Harry', last: 'Potter'},
+    {first: 'Hermione', last: 'Granger'},
+    {first: 'Ron', last: 'Weasley'},
+    {first: 'Rubeus', last: 'Hagrid'},
+    {first: 'Minerva', last: 'McGonagall'},
+    {first: 'Severus', last: 'Snape'}
+];
+
+console.log(fullNames);
+console.table(fullNames);
+
+const firstNames = fullNames.map(function(name) {
+    return name.first;
+});
+
+console.log(firstNames);
+console.table(firstNames);
+
+//
+
+// ARROW FUNCTIONS //
+
+// ARROW FUNCTION WITH TWO PARAMETERS
+const addMultiply = (x, y) => {
+    console.log((x + 2) * y);
+}
+addMultiply(10, 30);
+
+// ARROW FUNCTION WITH ONE PARAMETER
+const squareSquare = x => {
+    console.log(x * x);
+}
+squareSquare(9);
+
+// ARROW FUNCTION WITH NO PARAMETER
+const randomNumbers = () => {
+    console.log(Math.floor(Math.random() * 100) + 1);
+}
+randomNumbers();
+
+//
+const greet = (str) => {
+    return `Hey ${str}!`;
+}
+
+console.log(greet("Hagrid"));
+console.log(greet("Luna"));
+
+//
+
+//IMPLICIT RETURNS!!!!!!!!! BASICALLY YOU DONT NEED TO WRITE return, YOU CAN SIMPLY REPLACE THE CURLY BRACES WITH PARENTHESIS AND BOOM! IT WILL RETURN THE VALUE!!! WHAAAAAAAAAAT!?!
+
+//ONLY WORKS IF THERE IS ONE AND ONLY ONE STATEMENT
+
+//YOU ALSO DON'T NEED A ; AFTER THE STATEMENT
+
+const oneToFive = () => (
+    Math.floor(Math.random() * 5) + 1
+)
+
+console.log(oneToFive());
+
+//YOU CAN ALSO DO IT WITHOUT THE () OR {} AT ALL!!!!! JUST ONE SINGLE LINE OF CODE!!! STRAIGHT TO THE POINT!!!!!!!
+
+const subtract = (a, b) => a - b;
+
+console.log(subtract(20, 10));
+console.log(subtract(100, 50));
+
+//
+const movies = [
+    {
+        title: "The Devil Wears Prada",
+        score: 90
+    },
+    {
+        title: "Crazy Stupid Love",
+        score: 88
+    },
+    {
+        title: "Shrek",
+        score: 95
+    },
+    {
+        title: "Up",
+        score: 80
+    }
+]
+
+//FROM COMPLEX TO SINGLE
+
+//ONE
+// let titles = movies.map(function(movie) {
+//     return `${movie.title} -- ${movie.score / 10}`;
+// });
+//
+// console.log(titles);
+
+
+//TWO
+// let titles = movies.map((movie) => {
+//     return `${movie.title} -- ${movie.score / 10}`;
+// });
+//
+// console.log(titles);
+
+
+//THREE
+// let titles = movies.map(movie => {
+//     return `${movie.title} -- ${movie.score / 10}`;
+// });
+//
+// console.log(titles);
+
+
+//FOUR
+let titles = movies.map(movie => `${movie.title} -- ${movie.score / 10}`);
+console.log(titles);
